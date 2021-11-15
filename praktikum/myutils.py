703b2b76e150e9c31d53dfcb60c665a5c0a18547
FelixBieswanger <felix.bieswanger@googlemail.com>
2021-11-15 20:38:07 +0100

    add dataset class

--- praktikum/myutils.py
+++ praktikum/myutils.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import math
+import tensorflow as tf
 
 
 class Complexity_Measures:
@@ -34,6 +35,86 @@ class Complexity_Measures:
     def method2(data):
         return 3
 
+
+class Datasets:
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    def ALL_NUMBERS():
+        return Datasets.x_train, Datasets.y_train, Datasets.x_test, Datasets.y_test
+
+    def SIX_AND_NINES():
+        x_train = Datasets.x_train[(Datasets.y_train == 6) | (Datasets.y_train == 9)]
+        y_train = Datasets.y_train[(Datasets.y_train == 6) | (Datasets.y_train == 9)]
+        x_test = Datasets.x_test[(Datasets.y_test == 6) | (Datasets.y_test == 9)]
+        y_test = Datasets.y_test[(Datasets.y_test == 6) | (Datasets.y_test == 9)]
+        return x_train, y_train, x_test, y_test
+
+    def ONLY_SIXES():
+        """
+        only take 6
+        expected to have the lowest complexity
+        """
+        x_train = Datasets.x_train[Datasets.y_train == 6]
+        y_train = Datasets.y_train[Datasets.y_train == 6]
+        x_test = Datasets.x_test[Datasets.y_test == 6]
+        y_test = Datasets.y_test[Datasets.y_test == 6]
+        return x_train, y_train, x_test, y_test
+
+    def RANDOM():
+        x_train = np.array([np.random.rand(Datasets.x_train[0].shape[0], Datasets.x_train[0].shape[1]) * 255 for _ in range(len(Datasets.x_train))]).astype(int)
+        y_train = np.array([np.random.randint(0, 10) for _ in range(len(Datasets.x_train))])
+        x_test = np.array([np.random.rand(Datasets.x_test[0].shape[0], Datasets.x_test[0].shape[1]) * 255 for _ in range(len(Datasets.x_test))]).astype(int)
+        y_test = np.array([np.random.randint(0, 10) for _ in range(len(Datasets.x_test))])
+        return x_train, y_train, x_test, y_test
+
+    def SAMEPICTURE():
+        x_train = np.array([Datasets.x_train[0] for _ in range(len(Datasets.x_train))])
+        y_train = np.array([Datasets.y_train[0] for _ in range(len(Datasets.x_train))])
+        x_test = np.array([Datasets.x_test[0] for _ in range(len(Datasets.x_test))])
+        y_test = np.array([Datasets.y_test[0]for _ in range(len(Datasets.x_test))])
+        return x_train, y_train, x_test, y_test
+
+    def get_all_data():
+        all_num = Datasets.ALL_NUMBERS()
+        six_nines = Datasets.SIX_AND_NINES()
+        only_sixes = Datasets.ONLY_SIXES()
+        random = Datasets.RANDOM()
+        same_picture = Datasets.SAMEPICTURE()
+
+        return  {
+            "ALL_NUMBERS": {
+                "x_train": all_num[0],
+                "y_train": all_num[1],
+                "x_test": all_num[2],
+                "y_test": all_num[3]
+            
+            },
+            "SIX_AND_NINES": {
+                "x_train": six_nines[0],
+                "y_train": six_nines[1],
+                "x_test": six_nines[2],
+                "y_test": six_nines[3]
+            },
+            "ONLY_SIXES": {
+                "x_train": only_sixes[0],
+                "y_train": only_sixes[1],
+                "x_test": only_sixes[2],
+                "y_test": only_sixes[3]
+            },
+            "RANDOM": {
+                "x_train": random[0],
+                "y_train": random[1],
+                "x_test": random[2],
+                "y_test": random[3]
+            },
+            "SAMEPICTURE": {
+                "x_train": same_picture[0],
+                "y_train": same_picture[1],
+                "x_test": same_picture[2],
+                "y_test": same_picture[3]
+            }
+        }
+  
 
 class Helpers:
 
